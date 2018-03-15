1771cc8cd14566873627ab12c11b0e063c9484ec
evanthebouncy <evanthebouncy@gmail.com>
2018-03-15 12:32:47 -0400

    more changes, fixed a RELU -> Sigmoid bug

--- algebraic_model.py
+++ algebraic_model.py
@@ -8,6 +8,8 @@ import numpy as np
 import torch.nn.functional as F
 from constants import *
 
+large_hidden = 4 * n_hidden
+
 def to_torch(x):
   x = Variable(torch.from_numpy(x)).type(torch.cuda.FloatTensor)
   return x
@@ -20,19 +22,29 @@ class ANet(nn.Module):
     # 6 input image channel, 6 output channels, 2x2 square convolution
     self.enc_conv1 = nn.Conv2d(6, 12, 2)
     self.enc_conv2 = nn.Conv2d(12, 12, 2)
-    self.enc_fc1 = nn.Linear(192, n_hidden)
-    self.enc_fc2 = nn.Linear(n_hidden, n_hidden)
+    self.enc_fc1 = nn.Linear(192, large_hidden)
+    self.enc_fc2 = nn.Linear(large_hidden, n_hidden)
     # for decoding
-    self.dec_fc = nn.Linear(n_hidden, L*L*6)
+    self.dec_fc1 = nn.Linear(n_hidden, large_hidden)
+    self.dec_fc2 = nn.Linear(large_hidden, L*L*6)
     # for abstract functions
-    self.h_fc1 = nn.Linear(n_hidden + n_hidden, n_hidden + n_hidden)
-    self.h_fc2 = nn.Linear(n_hidden + n_hidden, n_hidden)
-    self.v_fc1 = nn.Linear(n_hidden + n_hidden, n_hidden + n_hidden)
-    self.v_fc2 = nn.Linear(n_hidden + n_hidden, n_hidden)
+    self.h_fc1 = nn.Linear(n_hidden + n_hidden, large_hidden)
+    self.h_fc2 = nn.Linear(large_hidden, n_hidden)
+    self.v_fc1 = nn.Linear(n_hidden + n_hidden, large_hidden)
+    self.v_fc2 = nn.Linear(large_hidden, n_hidden)
 
-#    self.meta_param = {
-#      'learning_rate' : 0.1,
-#    }
+    self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+    self.model_loc = './models/tan_algebra.mdl'
+    
+    # for book keeping
+    self.enc_dec_weights = [
+      self.enc_conv1,
+      self.enc_conv2,
+      self.enc_fc1,
+      self.enc_fc2,
+      self.dec_fc1,
+      self.dec_fc2,
+    ]
 
   def channel_last(self, x):
     '''
@@ -63,15 +75,15 @@ class ANet(nn.Module):
 
   def dec(self, x):
     # here x has dimension [batch x L*L*6]
-    x = self.dec_fc(x)
+    x = self.dec_fc1(x)
+    x = self.dec_fc2(x)
     # roll it back into [batch x L*L x 6]
     x = x.view(-1, L*L, 6)
     #print (x.size())
+    x = F.softmax(x, dim=2)
     # add a smol constant because I am paranoid
     smol_const = to_torch(np.array([1e-6]))
     x = x + smol_const.expand(x.size())
-
-    x = F.softmax(x, dim=2)
     #print (x.size())
     return x
 
@@ -79,14 +91,14 @@ class ANet(nn.Module):
     assert x1.size() == x2.size()
     x1x2 = torch.cat((x1,x2),1)
     x = F.relu(self.h_fc1(x1x2))
-    x = F.relu(self.h_fc2(x))
+    x = F.sigmoid(self.h_fc2(x))
     return x
 
   def abstr_v(self, x1, x2):
     assert x1.size() == x2.size()
     x1x2 = torch.cat((x1,x2),1)
     x = F.relu(self.v_fc1(x1x2))
-    x = F.relu(self.v_fc2(x))
+    x = F.sigmoid(self.v_fc2(x))
     return x
 
   def dec_to_board(self, x):
@@ -127,61 +139,154 @@ class ANet(nn.Module):
   def enc_dist(self, x, y):
     return torch.sum(torch.pow((x - y), 2))
 
-if __name__ == '__main__':
-
-  net = ANet().cuda()
-  #print(net)
-
-  optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-  model_loc = './models/tan_algebra.mdl'
-
-  for i in range(100000):
-    optimizer.zero_grad()
-
-    # train for the embedding cost
-    b = gen_train_embed_batch()
-    b = to_torch(b)
+  # strictly train the embedding batch
+  def train_embedding(self, bb):
+    self.optimizer.zero_grad()
+    b = to_torch(bb)
     # this embedding has shape [20 x 20] = [batch x hidden]
-    b_emb = net.enc(b)
+    b_emb = self.enc(b)
     # this reconstruction should have shape [20 x 6 * 6 x 6]
-    b_rec = net.dec(b_emb)
-    embed_cost = net.auto_enc_cost(b, b_rec)
+    b_rec = self.dec(b_emb)
+    embed_cost = self.auto_enc_cost(b, b_rec)
 
+    cost = embed_cost
+    cost.backward()
+    self.optimizer.step()
+    return cost
+
+  # strictly train the algebra aspect
+  def train_algebra(self, hv_batch, freeze=True):
+    self.optimizer.zero_grad()
     # train the algebraic cost
-    h_batch, v_batch = gen_train_compose_batch()
+    h_batch, v_batch = hv_batch
     # H operator
     h_arg1, h_arg2, h_result = h_batch
     h_arg1, h_arg2, h_result = to_torch(h_arg1), to_torch(h_arg2), to_torch(h_result)
-    h_pred = net.abstr_h(net.enc(h_arg1), net.enc(h_arg2))
-    h_result = net.enc(h_result)
-    h_cost = net.algebra_cost(h_result, h_pred)
+    h_pred = self.abstr_h(self.enc(h_arg1), self.enc(h_arg2))
+    h_pred_dec = self.dec(h_pred)
+    h_cost = self.auto_enc_cost(h_result, h_pred_dec)
+
+    h_emb_cost = self.algebra_cost(self.enc(h_result), h_pred)
 
     # V operator
     v_arg1, v_arg2, v_result = v_batch
     v_arg1, v_arg2, v_result = to_torch(v_arg1), to_torch(v_arg2), to_torch(v_result)
-    v_pred = net.abstr_v(net.enc(v_arg1), net.enc(v_arg2))
-    v_result = net.enc(v_result)
-    v_cost = net.algebra_cost(v_result, v_pred)
+    v_pred = self.abstr_v(self.enc(v_arg1), self.enc(v_arg2))
+    v_pred_dec = self.dec(v_pred)
+    v_cost = self.auto_enc_cost(v_result, v_pred_dec)
 
-    alg_cost = h_cost + v_cost
+    v_emb_cost = self.algebra_cost(self.enc(v_result), v_pred)
 
-    cost = embed_cost + alg_cost
+    rec_cost = h_cost + v_cost
+    emb_cost = h_emb_cost + v_emb_cost
+
+    cost = rec_cost + emb_cost
     cost.backward()
-    optimizer.step()
+
+    if freeze:
+      # freeze these weights
+      for param in self.enc_dec_weights:
+        param.zero_grad()
+      self.optimizer.step()
+
+    return rec_cost, emb_cost
+
+if __name__ == '__main__':
+
+  net = ANet().cuda()
+
+  # train the embeddings for some iterations
+  for i in range(10001):
+
+    # train for the embedding cost
+    cost = net.train_embedding(gen_train_embed_batch())
 
     if i % 1000 == 0:
-      print ("===== d i a g n o s t i c    a e s t h e t i c s ===== ", i)
+      print ("===== e m b e d d i n g    a e s t h e t i c s ===== ", i)
       print("cost ", cost)
-      print ('embed cost', embed_cost)
-      print ('alg_cost', alg_cost)
+      torch.save(net.state_dict(), net.model_loc) 
+      b = gen_train_embed_batch()
+      b = to_torch(b)
+      b_rec = net.dec(net.enc(b))
       for jjj in range(20):
         orig_board = net.dec_to_board(net.channel_last(b)[jjj])
         rec_board = net.dec_to_board(b_rec[jjj])
-        # print (orig_board)
-        # print (rec_board)
-        render_board(orig_board, "board_{}_orig.png".format(jjj))
-        render_board(rec_board, "board_{}_rec.png".format(jjj))
+        render_board(orig_board, "embed_board_{}_orig.png".format(jjj))
+        render_board(rec_board, "embed_board_{}_rec.png".format(jjj))
 
-        torch.save(net.state_dict(), model_loc) 
+  # train the algebra for some iterations
+  for i in range(10001):
+    if i < 10000 / 2:
+      # freeze the weight, trian the algebra hard
+      alg_cost = net.train_algebra(gen_train_compose_batch())
+    else:
+      # jointly train both the algebra and the embedding
+      alg_cost = net.train_algebra(gen_train_compose_batch(), freeze=False)
+      emb_cost = net.train_embedding(gen_train_embed_batch())
+
+    if i % 1000 == 0:
+      print ("===== a l g e b r a i c    a e s t h e t i c s ===== ", i)
+      print("cost ", alg_cost)
+      torch.save(net.state_dict(), net.model_loc) 
+
+      h_batch, v_batch = gen_train_compose_batch()
+      # H operator
+      h_arg1, h_arg2, h_result = h_batch
+      h_arg1, h_arg2, h_result = to_torch(h_arg1), to_torch(h_arg2), to_torch(h_result)
+      h_pred = net.abstr_h(net.enc(h_arg1), net.enc(h_arg2))
+      b_rec = net.dec(h_pred)
+
+      for jjj in range(len(h_arg1)):
+        orig_board = net.dec_to_board(net.channel_last(h_result)[jjj])
+        arg1_board = net.dec_to_board(net.channel_last(h_arg1)[jjj])
+        arg2_board = net.dec_to_board(net.channel_last(h_arg2)[jjj])
+        rec_board = net.dec_to_board(b_rec[jjj])
+        render_board(arg1_board, "algebra_board_{}_arg1.png".format(jjj))
+        render_board(arg2_board, "algebra_board_{}_arg2.png".format(jjj))
+        render_board(orig_board, "algebra_board_{}_result.png".format(jjj))
+        render_board(rec_board,  "algebra_board_{}_predict.png".format(jjj))
+
+# diagnosis
+#    max_element = 0.0
+#    max_grad = 0.0
+#    for param in net.parameters():
+#      try:
+#        x = param.data.cpu().numpy()
+#        x_grad = param.grad.data.cpu().numpy()
+#        max_element = max(max_element, np.max(np.abs(x)))
+#        max_grad = max(max_grad, np.max(np.abs(x_grad)))
+#      except: pass
+#    print ("cost {} max weight {} max grad {}".format(cost, max_element, max_grad))
+
+
+
+  # then train the algebra
+
+
+
+#    # train the algebraic cost
+#    h_batch, v_batch = gen_train_compose_batch()
+#    # H operator
+#    h_arg1, h_arg2, h_result = h_batch
+#    h_arg1, h_arg2, h_result = to_torch(h_arg1), to_torch(h_arg2), to_torch(h_result)
+#    h_pred = net.abstr_h(net.enc(h_arg1), net.enc(h_arg2))
+#    h_result_enc = net.enc(h_result)
+#    h_cost = net.algebra_cost(h_result_enc, h_pred)
+#
+#    # V operator
+#    v_arg1, v_arg2, v_result = v_batch
+#    v_arg1, v_arg2, v_result = to_torch(v_arg1), to_torch(v_arg2), to_torch(v_result)
+#    v_pred = net.abstr_v(net.enc(v_arg1), net.enc(v_arg2))
+#    v_result_enc = net.enc(v_result)
+#    v_cost = net.algebra_cost(v_result_enc, v_pred)
+#
+#    alg_cost = h_cost + v_cost
+#
+#    # put another auto-encoding cost for safety ...? decode both h_emb and v_emb
+#    h_pred_dec = net.dec(h_pred)
+#    h_embed_cost = net.auto_enc_cost(h_result, h_pred_dec)
+#    v_pred_dec = net.dec(v_pred)
+#    v_embed_cost = net.auto_enc_cost(v_result, v_pred_dec)
+#    hv_embed_cost = 0.1 * (h_embed_cost + v_embed_cost)
 
 
--- visualize.py
+++ visualize.py
@@ -2,12 +2,30 @@ from data import *
 from embed_mode import *
 from algebraic_model import *
 from sklearn.manifold import TSNE
+from data import *
 
-if __name__ == '__main__':
-  net = ANet().cuda()
-  model_loc = './models/tan_algebra.mdl'
-  net.load_state_dict(torch.load(model_loc))
+def visualize_algebra(net):
+  # train the algebraic cost
+  h_batch, v_batch = gen_train_compose_batch()
+  # H operator
+  h_arg1, h_arg2, h_result = h_batch
+  h_arg1, h_arg2, h_result = to_torch(h_arg1), to_torch(h_arg2), to_torch(h_result)
+  h_pred = net.abstr_h(net.enc(h_arg1), net.enc(h_arg2))
+  b_rec = net.dec(h_pred)
 
+  for jjj in range(len(h_arg1)):
+    orig_board = net.dec_to_board(net.channel_last(h_result)[jjj])
+    arg1_board = net.dec_to_board(net.channel_last(h_arg1)[jjj])
+    arg2_board = net.dec_to_board(net.channel_last(h_arg2)[jjj])
+    rec_board = net.dec_to_board(b_rec[jjj])
+    print (orig_board)
+    print (rec_board)
+    render_board(arg1_board, "board_{}_arg1.png".format(jjj))
+    render_board(arg2_board, "board_{}_arg2.png".format(jjj))
+    render_board(orig_board, "board_{}_truth.png".format(jjj))
+    render_board(rec_board, "board_{}_predict.png".format(jjj))
+
+def visualize_embedding(net):
   # generate 100 random tangrams
   b = gen_train_embed_batch(500)
   b = to_torch(b)    
@@ -32,4 +50,14 @@ if __name__ == '__main__':
 
   with open('data_vis.js', "w") as fd:
     fd.write('data_vis = ' + repr(dicc))
+
+
+
+if __name__ == '__main__':
+  net = ANet().cuda()
+  model_loc = './models/tan_algebra.mdl'
+  net.load_state_dict(torch.load(model_loc))
+  # visualize(net)
+  # visualize_embedding(net)
+  visualize_algebra(net)
 
